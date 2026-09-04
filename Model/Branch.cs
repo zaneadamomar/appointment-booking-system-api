d9e305dece020655d6f4152c08a23c2f25c8e7e8
Zane-Braxton <zane@braxton.co.za>
2026-09-04 17:13:49 +0200

    Branch isActive

--- Model/Branch.cs
+++ Model/Branch.cs
@@ -8,5 +8,6 @@
         public string? AddressLine2 { get; set; }
         public string? City { get; set; }
         public string? PostalCode { get; set; }
+        public bool IsActive { get; set; }
     }
 }
